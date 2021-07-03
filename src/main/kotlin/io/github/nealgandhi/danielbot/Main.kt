a7034b7229c1eea891c6a851dc184f69389317c8
Alex Habich <contact@alexhabi.ch>
2021-07-03 15:55:18 -0500

    chore: read from .env if present

--- src/main/kotlin/io/github/nealgandhi/danielbot/Main.kt
+++ src/main/kotlin/io/github/nealgandhi/danielbot/Main.kt
@@ -1,9 +1,10 @@
 package io.github.nealgandhi.danielbot
 
 import com.kotlindiscord.kord.extensions.ExtensibleBot
+import com.kotlindiscord.kord.extensions.utils.env
 
 suspend fun main() {
-    val token = System.getenv("DISCORD_TOKEN") ?: throw Exception("You must provide the DISCORD_TOKEN environment variable.")
+    val token = env("DISCORD_TOKEN") ?: throw Exception("You must provide the DISCORD_TOKEN environment variable.")
 
     val bot = ExtensibleBot(token) {
         slashCommands {
