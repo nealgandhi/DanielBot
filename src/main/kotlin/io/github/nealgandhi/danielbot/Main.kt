b8f8f0e1cb7e6988b00bfcc0df6ba60963a7805d
Alex Habich <contact@alexhabi.ch>
2021-07-03 15:57:29 -0500

    feat: register guild-only commands for development
    Providing the GUILD_ONLY_COMMANDS environment variable
    with the ID of a guild will automatically register all slash
    commands to that guild, rather than globally. This allows for
    much faster updating during development, but should not be
    used in production.

--- src/main/kotlin/io/github/nealgandhi/danielbot/Main.kt
+++ src/main/kotlin/io/github/nealgandhi/danielbot/Main.kt
@@ -2,6 +2,7 @@ package io.github.nealgandhi.danielbot
 
 import com.kotlindiscord.kord.extensions.ExtensibleBot
 import com.kotlindiscord.kord.extensions.utils.env
+import dev.kord.core.kordLogger
 
 suspend fun main() {
     val token = env("DISCORD_TOKEN") ?: throw Exception("You must provide the DISCORD_TOKEN environment variable.")
@@ -9,6 +10,10 @@ suspend fun main() {
     val bot = ExtensibleBot(token) {
         slashCommands {
             enabled = true
+            env("GUILD_ONLY_COMMANDS")?.let { guildId ->
+                kordLogger.warn("Registering commands as guild-only because the GUILD_ONLY_COMMANDS environment variable was set.")
+                defaultGuild(guildId)
+            }
         }
         extensions {
             add(::TestExtension)
